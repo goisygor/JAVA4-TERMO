ded2e11eb5eea8d70d0257be3ab0e0b007afdbd6
goisygor <ygorgoissenai@gmail.com>
2024-10-03 21:55:43 -0300

    ok

--- Documents/JAVA4°TERMO/JavaProjectsNoite-main/gui_exercicio1_calculadora_abas/src/main/java/com/example/CalculadoraSimples.java
+++ Documents/JAVA4°TERMO/JavaProjectsNoite-main/gui_exercicio1_calculadora_abas/src/main/java/com/example/CalculadoraSimples.java
@@ -3,9 +3,14 @@ package com.example;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import java.awt.*;;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CalculadoraSimples extends JPanel {
+    // ATRIBUTOS
     JTextField displaySimples;
     String[] botoes = { "7", "8", "9", "/",
             "4", "5", "6", "*",
@@ -20,19 +25,66 @@ public class CalculadoraSimples extends JPanel {
         displaySimples.setEditable(false);
         this.add(displaySimples, BorderLayout.NORTH);
 
-        // CRIAR UM PAINEL PARA OS BOTÕES
-        JPanel painelBotoes = new JPanel(new GridLayout(4, 4, 2, 2));
-        for (String textBotoes : botoes) {
-            JButton botao = new JButton(textBotoes);
-            // adicionar ação dos botões 
+        // Criar um Painel para os botões
+        JPanel painelBotoes = new JPanel(new GridLayout(4, 4, 3, 3));
+        for (String textoBotoes : botoes) {
+            JButton botao = new JButton(textoBotoes);
+            botao.addActionListener(new Handler1());
             painelBotoes.add(botao);
         }
         this.add(painelBotoes, BorderLayout.CENTER);
     }
-    public void setDisplaySimples(String texto){
+
+    // altera valor display
+    public void setDisplay(String texto) {
         this.displaySimples.setText(texto);
     }
-    public String getDisplaySimples(){
-        return this.displaySimples.getText();
+
+    // pega valor display
+    public String getDisplay() {
+        return displaySimples.getText();
+    }
+
+    public class Handler1 implements ActionListener {
+        double valorAtual;
+        String operador;
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            String comando = e.getActionCommand();
+
+            
+            if (comando.matches("\\d")) {
+                displaySimples.setText(displaySimples.getText() + comando);
+            } else if (comando.matches("[+\\-*/]")) {
+                valorAtual = Double.parseDouble(displaySimples.getText());
+                operador = comando;
+                displaySimples.setText("");
+            } else if (comando.equals("=")) {
+                double segundoValor = Double.parseDouble(displaySimples.getText());
+                double resultado = 0;
+
+                switch (operador) {
+                    case "+":
+                        resultado = valorAtual + segundoValor;
+                        break;
+                    case "-":
+                        resultado = valorAtual - segundoValor;
+                        break;
+                    case "*":
+                        resultado = valorAtual * segundoValor;
+                        break;
+                    case "/":
+                        resultado = valorAtual / segundoValor;
+                        break;
+                }
+                displaySimples.setText(String.valueOf(resultado));
+            } else if (comando.equals("C")) {
+                displaySimples.setText("");
+                valorAtual = 0;
+            }
+
+        }
     }
 }
